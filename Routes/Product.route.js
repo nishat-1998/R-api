12a75ba49e6181d58238678fa0d20be0156c548c
Nishat Ahmed <nishatahmed4485@gmail.com>
2021-12-27 18:11:50 +0600

    update

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -2,41 +2,79 @@ const express =require('express');
 const router = express.Router();
 
 
-const Product = require('../Models/Product.model')
+const Product = require('../Models/Product.model');
 
-router.get('/',(req,res,next)=>{
-    next(new Error(" can not get a list"))
-   // res.send('getting a list of all products..');
+
+//get a list of all products
+router.get('/',async (req,res,next)=>{
+   
+try{
+    const results = await Product.find({},{ __v: 0});
+   // const results = await Product.find({},{ name:1,price:1,available:1, _id:0 });
+ // const results = await Product.find({price:6999},{});
+   res.send(results)
+}catch (error) {
+console.log(error.message);
+}
     
 });
 
-
-router.post('/',(req,res,next)=>{
-    console.log(req.body);
-    const product = new Product({
-        name:req.body.name,
-        price:req.body.price,
-        available:req.body.available
-    })
-
-    product.save()
-    .then(result =>{
-        console.log(result);
+// create a new product
+router.post('/',async(req,res,next)=>{
+  
+    try{
+        const product = new Product(req.body)
+        const result = await product.save()
         res.send(result)
-    })
-    .catch(err =>{
-        console.log(err.message);
-    })
+    }catch (error) {
+   console.log(error.message);
+    }
+
+
+  
+    //  console.log(req.body);
+    //const product = new Product({
+    //     name:req.body.name,
+    //     price:req.body.price,
+    //     available:req.body.available
+    // });
+
+    // product.save()
+    // .then(result =>{
+    //     console.log(result);
+    //     res.send(result)
+    // })
+    // .catch(err =>{
+    //     console.log(err.message);
+    // })
   
 });
+// Get a product by id 
+router.get('/:id',async(req,res,next)=> {
+const id = req.params.id;
+try{
+   // const product = await Product.findById(id);
+   const product = await Product.findOne({_id:id})
+    res.send(product);
+}catch (error) {
+console.log(error.message);
+}
+});
 
-router.get('/:id',(req,res,next)=> {
-res.send("single product")
-})
+//update a product by id
 router.patch('/:id',(req,res,next)=> {
     res.send('updating product')
 })
-router.delete('/:id',(req,res,next)=> {
+
+router.delete('/:id', async(req,res,next)=> {
+    const id= req.params.id
+    try{
+       const result = await Product.findByIdAndDelete(id); 
+       console.log(result)
+        res.send(result)
+    }catch (error) {
+   console.log(error.message);
+    } 
     res.send('deleting product')
 })
 
